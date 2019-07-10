50d7388ee1298ad4f568c268a6d1dcb77c8760a7
fengchen <xie_cang@163.com>
2019-07-10 08:33:02 +0800

    将 player 抽象成一个类并可以移动

--- guagame/gua_game.js
+++ guagame/gua_game.js
@@ -31,12 +31,13 @@ class GuaGame {
     }
     // 这里如果不使用箭头函数就要在前面 bind
     update() {
-        this.scene.upate()
+        this.scene.update()
     }
     draw() {
         this.scene.draw()
     }
     registerAction(key, callback) {
+        log('registerAction: ', key)
         this.actions[key] = callback
     }
     runloop() {
@@ -52,13 +53,11 @@ class GuaGame {
             }
         }
         // update 更新
-        // g.update()
-        g.scene.update()
+        g.update()
         // clear 清除画板
         g.context.clearRect(0, 0, g.canvas.width, g.canvas.height)
         // draw 画图
-        // g.draw()
-        g.scene.draw()
+        g.draw()
         // next run loop
         // 递归调用
         setTimeout(function () {
@@ -112,7 +111,7 @@ class GuaGame {
                 // 所有图片载入后执行，调用 run
                 loads.push(1)
                 // log('载入图片', loads.length, names.length)
-                if (loads.length == names.length) {
+                if (loads.length === names.length) {
                     // g.run()
                     g.__start()
                 }
@@ -120,118 +119,3 @@ class GuaGame {
         }
     }
 }
-
-// 一个 guagame 对象，用来加载画布
-// let GuaGame = function (fps, images, runCallback) {
-//     // images 是一个对象，里边是图片的名字
-//     // 程序会在所有图片载入成功后运行
-//     let g = {
-//         // 储存 key 和 function
-//         actions: {},
-//         // 储存按键的状态
-//         keydowns: {},
-//         // 图片
-//         images: {}
-//     }
-//     // 拿到 canvas
-//     let canvas = document.querySelector('#id-canvas')
-//     // 在 canvas 上指定 2d 图
-//     let context = canvas.getContext('2d')
-
-//     g.canvas = canvas
-//     g.context = context
-
-//     // draw
-//     g.drawImage = function (guaImage) {
-//         g.context.drawImage(guaImage.image, guaImage.x, guaImage.y)
-//     }
-
-//     // event 按键状态,在 keydowns 储存
-//     window.addEventListener('keydown', function (event) {
-//         g.keydowns[event.key] = true
-//     })
-//     window.addEventListener('keyup', function (event) {
-//         g.keydowns[event.key] = false
-//     })
-//     // 注册时间，按了那个 key 要执行什么函数, 在 setInterval 中调用
-//     g.registerAction = function (key, callback) {
-//         g.actions[key] = callback
-//     }
-
-//     // timer
-//     window.fps = 30
-//     let runloop = function() {
-//         // log(window.fps)
-//         // events
-//         let actions = Object.keys(g.actions)  // 获取到所有的 actions
-//         for (let i = 0; i < actions.length; i++) {
-//             let key = actions[i]
-//             if (g.keydowns[key]) {
-//                 // 如果按键按下，调用注册的 action 的 callback
-//                 g.actions[key]()
-//             }
-//         }
-//         // update 更新
-//         // g.update()
-//         g.scene.update()
-//         // clear 清除画板
-//         context.clearRect(0, 0, canvas.width, canvas.height)
-//         // drew 画图
-//         // g.draw()
-//         g.scene.draw()
-//         // next run loop
-//         // 递归调用
-//         setTimeout(function () {
-//             runloop()
-//         }, 1000 / window.fps)
-//     }
-//     let loads = []
-//     // 预先载入所有图片
-//     let names = Object.keys(images)
-//     for (let i = 0; i < names.length; i++) {
-//         let name = names[i]
-//         let path = images[name]
-//         let img = new Image()
-//         img.src = path
-//         img.onload = function() {
-//             // 存入 g.image 中
-//             g.images[name] = img
-//             // 所有图片载入后执行，调用 run
-//             loads.push(1)
-//             // log('载入图片', loads.length, names.length)
-//             if (loads.length == names.length) {
-//                 // g.run()
-//                 g.__start()
-//             }
-//         }
-//     }
-//     g.imageByName= function(name) {
-//         let img = g.images[name]
-//         let image = {
-//             w: img.width,
-//             h: img.height,
-//             image: img,
-//         }
-//         return image
-//     }
-//     g.runWithScene = function(scene) {
-//         g.scene = scene
-//         // 开始运行程序
-//         setTimeout(function () {
-//             runloop()
-//         }, 1000 / fps)
-//     }
-//     g.replaceScene = function(scene) {
-//         g.scene = scene
-//     }
-//     g.__start = function() {
-//         // 第一次运行需要加上 runCallback
-//         runCallback(g)
-//         // 开始运行程序
-//         // setTimeout(function () {
-//         //     runloop()
-//         // }, 1000 / fps)
-//         // return g
-//     }
-//     return g
-// }
--- scene/main/scene.js
+++ scene/main/scene.js
@@ -1,21 +1,66 @@
+class Player extends GuaImage {
+    constructor(game) {
+        super(game, 'player')
+        this.speed = 10
+    }
+    update() {
+
+    }
+    moveLeft() {
+        this.x -= this.speed
+    }
+    moveRight() {
+        this.x += this.speed
+    }
+    moveUp() {
+        this.y -= this.speed
+    }
+    moveDown() {
+        this.y += this.speed
+    }
+}
+
 class Scene extends GuaScene {
     constructor(game) {
         super(game)
         this.setup()
+        this.setupInputs()
     }
     setup() {
         this.bg = GuaImage.new(this.game, 'sky')
-        this.player = GuaImage.new(this.game, 'player')
+        this.player = Player.new(this.game)
         this.player.x = 100
         this.player.y = 200
         this.cloud = GuaImage.new(this.game, 'cloud')
 
-        // 进一步提取
         this.addElement(this.bg)
         this.addElement(this.player)
         this.addElement(this.cloud)
+        this.addElement(this.player)
     }
     update() {
         this.cloud.y += 1
+    }
+    setupInputs() {
+        let g = this.game
+        let s = this
+
+        log('g: ', g, 's: ', s, 'this: ', this)
+        // 这里只能这样写, 注释的不能运行 TypeError: this.player is undefined
+        // this.game.registerAction('a', function () {
+        //     this.player.moveLeft()
+        // })
+        g.registerAction('a', function () {
+            s.player.moveLeft()
+        })
+        g.registerAction('d', function () {
+            s.player.moveRight()
+        })
+        g.registerAction('w', function () {
+            s.player.moveUp()
+        })
+        g.registerAction('s', function () {
+            s.player.moveDown()
+        })
     }
 }
