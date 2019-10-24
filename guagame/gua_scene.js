53c24bab85f880a4701b5ccd628b776d0a847fb7
fengchen <xie_cang@163.com>
2019-10-24 22:17:41 +0800

    update core

--- guagame/gua_scene.js
+++ guagame/gua_scene.js
@@ -5,21 +5,45 @@ class GuaScene {
         this.elements = []
     }
 
+    // 将子类中的共同方法提取到父类中
     static new(game) {
-        let i = new this(game)
-        return i
-    }
-
-    draw() {
-        for (let i = 0; i < this.elements.length; i++) {
-            let e = this.elements[i]
-            this.game.drawImage(e)
-        }
+        return new this(game)
     }
 
     addElement(img) {
         img.scene = this
         this.elements.push(img)
+    }
+
+    deleteElement(img) {
+        img.scene = this
+        let index = this.elements.indexOf(img)
+        this.elements.splice(index, 1)
+    }
+
+
+    drawRect(x, y, w, h, color) {
+        let self = this
+        let context = this.game.context
+
+        context.fillStyle = color
+        context.fillRect(x, y, w, h)
+    }
+
+    drawLabel(text, x, y, color, fontSize) {
+        let context = this.game.context
+
+        // log('drawLabel', x, y)
+        context.font = fontSize + "px serif";
+        context.fillStyle = color
+        context.fillText(text, x, y)
+    }
+
+    draw() {
+        for (let i = 0; i < this.elements.length; i++) {
+            let e = this.elements[i]
+            e.draw()
+        }
     }
 
     update() {
